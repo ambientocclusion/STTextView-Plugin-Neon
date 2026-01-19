b84fe1475cf347d2c83ec480f1fc3c5899288030
ambientocclusion <1399123+ambientocclusion@users.noreply.github.com>
2026-01-19 18:01:51 +0900

    Update Coordinator.swift

--- Sources/STPluginNeonAppKit/Coordinator.swift
+++ Sources/STPluginNeonAppKit/Coordinator.swift
@@ -38,6 +38,7 @@ public class Coordinator {
 
         // set textview default font to theme default font
         textView.font = theme.font(forToken: "plain") ?? textView.font
+        textView.textColor = theme.color(forToken: "plain") ?? textView.textColor
 
         highlighter = Neon.Highlighter(textInterface: STTextViewSystemInterface(textView: textView) { neonToken in
             var attributes: [NSAttributedString.Key: Any] = [:]
